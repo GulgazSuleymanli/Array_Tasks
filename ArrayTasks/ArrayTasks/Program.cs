34f9910c4ba0a9adeb86688328682826f3fe0ebb
GulgazSuleymanli <bd7tk5y26@code.edu.az>
2023-09-28 22:05:48 +0400

    Completed ArrayTasks

---
+++ ArrayTasks/ArrayTasks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ArrayTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            #region task1
+            //int[] numbers1 = { 4, 5, 2, 6, 4, 3 };
+            //int[] numbers2 = { 7, 4, 2, 76, 45, 65 };
+            //int count;
+            //for (int i = 0; i < numbers1.Length; i++)
+            //{
+            //    count = 0;
+            //    for (int j = 0; j < numbers2.Length; j++)
+            //    {
+            //        if (numbers1[i] == numbers2[j])
+            //        {
+            //            count++;
+            //            break;
+            //        }
+            //    }
+            //    if(count==0)
+            //    {
+            //        Console.WriteLine(numbers1[i]);
+            //    }
+            //}
+            #endregion
+
+            #region task2
+            //int[] arr1= {1,2,3,4,5};
+            //int[] arr2 = new int[arr1.Length];
+            //for(int i = 0; i < arr1.Length; i++)
+            //{
+            //    arr2[i] = arr1[arr1.Length-1-i];
+            //}
+            //for(int i = 0;i < arr2.Length; i++)
+            //{
+            //    Console.WriteLine(arr2[i]);
+            //}
+            #endregion
+
+
+
+
+        }
+    }
+}
